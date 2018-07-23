67d86904e889305c91dfe452230ab339f438f913
Emily <esilgard@users.noreply.github.com>
2018-07-23 15:29:00 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='hutchner',
+    version='0.2.2',
+    include_package_data = True,
+    packages=find_packages(),
+)
